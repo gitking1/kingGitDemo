9bf0ce248cf5ab60096cdc465350cda6489c33fb
kanishk <azad.kashu@gmail.com>
2024-04-24 17:40:00 +0530

    commit by Y guy

--- CoreJava/src/corejava/ArraysDemo.java
+++ CoreJava/src/corejava/ArraysDemo.java
@@ -16,6 +16,7 @@ public class ArraysDemo {
 			System.out.println(a[i]);
 		}
 		System.out.println("*****************");
+		System.out.println("changes by git Y");
 		int b[] = { 1, 4, 3, 5, 7, 8 };
 		for (int i = 0; i < b.length; i++) {
 			System.out.println(b[i]);
